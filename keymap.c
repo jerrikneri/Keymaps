5323241232595e21b88a1784e5884a5af288a125
Jerrik <jerrik+intelmac@sendlane.com>
2018-10-17 16:07:39 -0700

    Add spotlight macro to lower layer.

--- keymap.c
+++ keymap.c
@@ -30,6 +30,7 @@
 #define KC_PF LGUI(KC_RBRC)             // Page forward
 #define KC_SF LGUI(KC_GRV)              // Switch browser focus
 #define KC_X0 MT(MOD_LCTL, KC_ESC)
+#define KC_SPLT LGUI(KC_SPC)
 // #define KC_X1 LOWER
 // #define KC_X2 RAISE
 // #define KC_X3 LT(_FN3, KC_GRV)
@@ -127,7 +128,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = {
   {KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_EQL},
-  {_______, KC_CAPP, KC_LEFT, KC_SSL,  KC_SSR,  KC_LBRC, KC_RBRC, KC_4,    KC_5,    KC_6,    KC_PLUS, KC_PIPE},
+  {KC_SPLT, KC_CAPP, KC_LEFT, KC_SSL,  KC_SSR,  KC_LBRC, KC_RBRC, KC_4,    KC_5,    KC_6,    KC_PLUS, KC_PIPE},
   {_______, KC_CPYP,  KC_SU,  KC_PB , KC_PF,  KC_LCBR,  KC_RCBR,  KC_1,    KC_2,    KC_3,    KC_MINS,   _______},
   {_______, _______, _______, _______, _______, _______, KC_DEL, _______, KC_0,  KC_DOT, _______, _______}
 },
