75a849c5ba5f44a4a67fdc3a78ef7d643b0af33d
Jerrik <jerrikn@me.com>
2020-08-25 19:30:49 -0700

    keymap updates

--- letssplit/keymap.c
+++ letssplit/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
   KC_X0,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_X4 , \
-  KC_GRV, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_BSPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+  KC_GRV, KC_LCTL, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_BSPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
 ),
 
 [_LOWER] = LAYOUT_ortho_4x12( \
@@ -49,8 +49,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_RAISE] = LAYOUT_ortho_4x12( \
   KC_GRV, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN , KC_PLUS, \
-  _______,  KC_MPRV, KC_MNXT, KC_VOLU, KC_PGUP, KC_HOME, KC_EQL,  KC_SF,   KC_UP  ,  KC_STL, KC_STR  , KC_BSLS, \
-  _______, KC_MSTP, KC_MPLY, KC_VOLD, KC_PGDN, KC_END,  KC_PLUS, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______, \
+  _______,  KC_MPRV, KC_MNXT, KC_VOLU, KC_PGUP, KC_HOME, KC_EQL,  KC_SF,  _______,  KC_STL, KC_STR  , KC_BSLS, \
+  _______, KC_MSTP, KC_MPLY, KC_VOLD, KC_PGDN, KC_END,  KC_PLUS, _______, _______,   KC_PB, KC_PF, _______, \
   KC_DEVT, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD , KC_VOLU, KC_MPLY \
 ),
 
--- planck/keymap.c
+++ planck/keymap.c
@@ -52,8 +52,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_RAISE] = LAYOUT_planck_grid(
   KC_GRV, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN , KC_PLUS,
-  _______,  KC_MPRV, KC_MNXT, KC_VOLU, KC_PGUP, KC_HOME, KC_EQL,  KC_SF,   KC_UP  ,  KC_STL, KC_STR  , KC_BSLS,
-  _______, KC_MSTP, KC_MPLY, KC_VOLD, KC_PGDN, KC_END,  KC_PLUS, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______,
+  _______,  KC_MPRV, KC_MNXT, KC_VOLU, KC_PGUP, KC_HOME, KC_EQL,  KC_SF,  _______, KC_STL, KC_STR  , KC_BSLS,
+  _______, KC_MSTP, KC_MPLY, KC_VOLD, KC_PGDN, KC_END,  KC_PLUS, _______, _______, KC_PB,  KC_PF   , _______,
   KC_DEVT, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD , KC_VOLU, KC_MPLY
 ),
 
