9e12703b03589dfe1a94cff40a212c249d6a2cc2
Jerrik <jerrikn@me.com>
2020-08-25 20:11:11 -0700

    clean up

--- letssplit/keymap.c
+++ letssplit/keymap.c
@@ -3,6 +3,7 @@
 #define _QWERTY 0
 #define _LOWER 1
 #define _RAISE 2
+#define _FUNCTION 3
 #define _ADJUST 16
 
 enum custom_keycodes {
@@ -12,6 +13,7 @@ enum custom_keycodes {
 #define LOWER  MO(_LOWER)
 #define RAISE  MO(_RAISE)
 #define ADJUST MO(_ADJUST)
+#define FUNCTION MO(_FUNCTION)
 
 #define KC_CAPW LGUI(LSFT(KC_3))        // Capture whole screen
 #define KC_CPYW LGUI(LSFT(LCTL(KC_3)))  // Copy whole screen
@@ -29,38 +31,45 @@ enum custom_keycodes {
 #define KC_X0 MT(MOD_LCTL, KC_ESC)      // Toggle CTRL/Esc
 #define KC_SPLT LGUI(KC_SPC)            // Alt spotlight/alfred
 #define KC_DEVT LGUI(LALT(KC_J))        // Toggle Dev Tools
+#define KC_DW LALT(KC_BSPC)             // Delete word
 #define KC_X4 MT(MOD_LSFT, KC_ENT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-[_QWERTY] = LAYOUT_ortho_4x12(
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
-  KC_X0,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_X4 , \
-  KC_GRV, KC_LCTL, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_BSPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
-),
+  [_QWERTY] = LAYOUT_ortho_4x12(
+    KC_TAB  , KC_Q   , KC_W   , KC_E   , KC_R,  KC_T  , KC_Y   , KC_U , KC_I   , KC_O   , KC_P   , KC_MINS, \
+    KC_X0   , KC_A   , KC_S   , KC_D   , KC_F,  KC_G  , KC_H   , KC_J , KC_K   , KC_L   , KC_SCLN, KC_QUOT, \
+    KC_LSFT , KC_Z   , KC_X   , KC_C   , KC_V,  KC_B  , KC_N   , KC_M , KC_COMM, KC_DOT , KC_SLSH, KC_X4  , \
+    FUNCTION, KC_LCTL, KC_LGUI, KC_LALT, LOWER, KC_SPC, KC_BSPC, RAISE, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT  \
+  ),
 
-[_LOWER] = LAYOUT_ortho_4x12( \
-  KC_TILD,  KC_1  ,  KC_2  ,  KC_3 ,  KC_4  ,   KC_5  ,  KC_6   ,  KC_7  ,   KC_8,  KC_9  , KC_0   , KC_EQL , \
-  KC_SPLT, KC_CAPP, _______, KC_SSL,  KC_SSR,  KC_LBRC, KC_RBRC ,  KC_4  ,   KC_5,  KC_6  , KC_PLUS, KC_PIPE, \
-  _______, KC_PB,   KC_PF,   KC_SU , KC_SD  ,  KC_LCBR,  KC_RCBR,  KC_1  ,   KC_2,  KC_3  , KC_MINS, _______, \
-  KC_DEVT, _______, _______, _______, _______, _______, KC_DEL  , _______,   KC_0,  KC_DOT, _______, _______ \
-),
+  [_LOWER] = LAYOUT_ortho_4x12( \
+    KC_TILD, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8,  KC_9  , KC_0   , KC_EQL , \
+    KC_SPLT, KC_CAPP, _______, KC_SSL , KC_SSR , KC_LBRC, KC_RBRC, KC_4   , KC_5,  KC_6  , KC_PLUS, KC_PIPE, \
+    _______, KC_PB  , KC_PF  , KC_SU  , KC_SD  , KC_LCBR, KC_RCBR, KC_1   , KC_2,  KC_3  , KC_MINS, _______, \
+    KC_DEVT, _______, _______, _______, _______, _______, KC_DW  , _______, KC_0,  KC_DOT, _______, _______  \
+  ),
 
-[_RAISE] = LAYOUT_ortho_4x12( \
-  KC_GRV, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN , KC_PLUS, \
-  _______,  KC_MPRV, KC_MNXT, KC_VOLU, KC_PGUP, KC_HOME, KC_EQL,  KC_SF,  _______,  KC_STL, KC_STR  , KC_BSLS, \
-  _______, KC_MSTP, KC_MPLY, KC_VOLD, KC_PGDN, KC_END,  KC_PLUS, _______, _______,   KC_PB, KC_PF, _______, \
-  KC_DEVT, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD , KC_VOLU, KC_MPLY \
-),
+  [_RAISE] = LAYOUT_ortho_4x12( \
+    KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PLUS, \
+    _______, KC_GRV , _______, _______, KC_PGUP, KC_HOME, KC_EQL , KC_SF  , _______, KC_STL , KC_STR , KC_BSLS, \
+    _______, _______, _______, _______, KC_PGDN, KC_END , KC_PLUS, _______, _______, KC_PB  , KC_PF  , _______, \
+    KC_DEVT, _______, _______, _______, _______, KC_TAB , _______, _______, _______, _______, _______, _______  \
+  ),
 
-[_ADJUST] =  LAYOUT_ortho_4x12( \
-  KC_F11, KC_F1  ,  KC_F2 , KC_F3  , KC_F4 , KC_F5   , KC_F6  , KC_F7 , KC_F8   , KC_F9  , KC_F10 , KC_F12 , \
-  _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  _______, _______,  _______, _______, \
-  _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______, \
-  RESET , _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
-)
+  [_FUNCTION] =  LAYOUT_ortho_4x12( \
+    KC_F11,  KC_F1  ,  KC_F2 , KC_F3  , KC_F4 ,  KC_F5  , KC_F6  , KC_F7  , KC_F8   , KC_F9 , KC_F10 , KC_F12 , \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY  \
+  ),
 
+  [_ADJUST] =  LAYOUT_ortho_4x12( \
+    _______, QWERTY , _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    RESET  , _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
+  )
 
 };
 
